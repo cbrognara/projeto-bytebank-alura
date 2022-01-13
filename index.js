f3787cdc9a43471b5d266dd9c67317f67aad798a
Camila Brognara <cbrognara@gmail.com>
2022-01-13 15:31:54 -0300

    criado aviso para impedir criacao de novas Contas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Cliente } from './cliente.js'
 import { ContaPoupanca } from './conta-poupanca.js'
 import { ContaCorrente } from './conta-corrente.js'
+import { Conta } from './conta.js'
 
 const cliente1 = new Cliente('Ricardo', 11122233309)
 
@@ -11,6 +12,9 @@ contaCorrenteRicardo.sacar(100)
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 contaPoupanca.sacar(10)
 
-console.log(contaCorrenteRicardo)
-console.log(contaPoupanca)
+// console.log(contaCorrenteRicardo)
+// console.log(contaPoupanca)
 // console.log(cliente1)
+
+const conta = new Conta(0, cliente1, 1001)
+console.log(conta)
